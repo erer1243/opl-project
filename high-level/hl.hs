e7b220f7e121c80a7fe472d0a208ce914827422f
erer1243 <>
2021-09-02 21:56:16 -0400

    completed task 3 - j0 test suite

--- high-level/hl.hs
+++ high-level/hl.hs
@@ -5,5 +5,20 @@ j0pp (J0Num n) = show n
 j0pp (J0Plus lhs rhs) = "(+ " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
 j0pp (J0Mult lhs rhs) = "(* " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
 
+-- [(program, expected_answer)]
+j0tests :: [(J0Expr, Integer)]
+j0tests = [ (J0Num 1, 1)
+          , (J0Num (-1), -1)
+          , (J0Plus (J0Num 1) (J0Num 1), 2)
+          , (J0Mult (J0Num 2) (J0Num 3), 6)
+          , (J0Mult (J0Num (-1)) (J0Num 3), -3)
+          , (J0Mult (J0Plus (J0Num 1) (J0Num 10)) (J0Num 3), 33)
+          , (J0Mult (J0Plus (J0Num 1) (J0Num (-1))) (J0Num 3), 0)
+          , (J0Plus (J0Plus (J0Num 1) (J0Num (-1))) (J0Num 3), 3)
+          , (J0Plus (J0Mult (J0Num 1) (J0Num (-1))) (J0Num 3), 2)
+          , (J0Mult (J0Plus (J0Plus (J0Num 5) (J0Num 10)) (J0Num (-1))) (J0Num 2), 28)
+          , (J0Mult (J0Mult (J0Mult (J0Num 2) (J0Num 2)) (J0Num 2)) (J0Num 2), 16)
+          , (J0Mult (J0Plus (J0Num 10) (J0Num 10)) (J0Mult (J0Num 2) (J0Num 2)), 80) ]
+
 main :: IO ()
 main = putStrLn $ j0pp $ J0Plus (J0Num 1) (J0Mult (J0Num 2) (J0Num 3))
