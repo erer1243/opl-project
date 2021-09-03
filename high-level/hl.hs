969e001a388a29a782e56db71d61d3676f943682
erer1243 <no email>
2021-09-03 08:53:15 -0400

    Removed j0 prefix from symbols

--- high-level/hl.hs
+++ high-level/hl.hs
@@ -1,39 +1,39 @@
-data J0Expr = J0Num Integer | J0Plus J0Expr J0Expr | J0Mult J0Expr J0Expr
+data JExpr = JNum Integer | JPlus JExpr JExpr | JMult JExpr JExpr
 
-j0pp :: J0Expr -> String
-j0pp (J0Num n) = show n
-j0pp (J0Plus lhs rhs) = "(+ " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
-j0pp (J0Mult lhs rhs) = "(* " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
+pp :: JExpr -> String
+pp (JNum n) = show n
+pp (JPlus lhs rhs) = "(+ " ++ pp lhs ++ " " ++ pp rhs ++ ")"
+pp (JMult lhs rhs) = "(* " ++ pp lhs ++ " " ++ pp rhs ++ ")"
 
 -- [(program, expected_answer)]
-j0tests :: [(J0Expr, Integer)]
-j0tests = [ (J0Num 1, 1)
-          , (J0Num (-1), -1)
-          , (J0Plus (J0Num 1) (J0Num 1), 2)
-          , (J0Mult (J0Num 2) (J0Num 3), 6)
-          , (J0Mult (J0Num (-1)) (J0Num 3), -3)
-          , (J0Mult (J0Plus (J0Num 1) (J0Num 10)) (J0Num 3), 33)
-          , (J0Mult (J0Plus (J0Num 1) (J0Num (-1))) (J0Num 3), 0)
-          , (J0Plus (J0Plus (J0Num 1) (J0Num (-1))) (J0Num 3), 3)
-          , (J0Plus (J0Mult (J0Num 1) (J0Num (-1))) (J0Num 3), 2)
-          , (J0Mult (J0Plus (J0Plus (J0Num 5) (J0Num 10)) (J0Num (-1))) (J0Num 2), 28)
-          , (J0Mult (J0Mult (J0Mult (J0Num 2) (J0Num 2)) (J0Num 2)) (J0Num 2), 16)
-          , (J0Mult (J0Plus (J0Num 10) (J0Num 10)) (J0Mult (J0Num 2) (J0Num 2)), 80) ]
+tests :: [(JExpr, Integer)]
+tests = [ (JNum 1, 1)
+        , (JNum (-1), -1)
+        , (JPlus (JNum 1) (JNum 1), 2)
+        , (JMult (JNum 2) (JNum 3), 6)
+        , (JMult (JNum (-1)) (JNum 3), -3)
+        , (JMult (JPlus (JNum 1) (JNum 10)) (JNum 3), 33)
+        , (JMult (JPlus (JNum 1) (JNum (-1))) (JNum 3), 0)
+        , (JPlus (JPlus (JNum 1) (JNum (-1))) (JNum 3), 3)
+        , (JPlus (JMult (JNum 1) (JNum (-1))) (JNum 3), 2)
+        , (JMult (JPlus (JPlus (JNum 5) (JNum 10)) (JNum (-1))) (JNum 2), 28)
+        , (JMult (JMult (JMult (JNum 2) (JNum 2)) (JNum 2)) (JNum 2), 16)
+        , (JMult (JPlus (JNum 10) (JNum 10)) (JMult (JNum 2) (JNum 2)), 80) ]
 
-j0interp :: J0Expr -> Integer
-j0interp (J0Num n) = n
-j0interp (J0Plus lhs rhs) = j0interp lhs + j0interp rhs
-j0interp (J0Mult lhs rhs) = j0interp lhs * j0interp rhs
+interp :: JExpr -> Integer
+interp (JNum n) = n
+interp (JPlus lhs rhs) = interp lhs + interp rhs
+interp (JMult lhs rhs) = interp lhs * interp rhs
 
-j0check :: J0Expr -> Integer -> Bool
-j0check expr ans = j0interp expr == ans
+check :: JExpr -> Integer -> Bool
+check expr ans = interp expr == ans
 
-j0runTask3Tests :: IO ()
-j0runTask3Tests = do
-    let testResults = map (uncurry j0check) j0tests
+runTests :: IO ()
+runTests = do
+    let testResults = map (uncurry check) tests
     let numSuccesses = length $ filter id testResults
-    let numFailures = length j0tests - numSuccesses
+    let numFailures = length tests - numSuccesses
     putStrLn $ show numSuccesses ++ " successes and " ++ show numFailures ++ " failures"
 
 main :: IO ()
-main = j0runTask3Tests
+main = runTests
