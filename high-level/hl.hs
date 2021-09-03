982f5dbb6a1b4d1ed2ef2a7ab04fe0615604f891
erer1243 <>
2021-09-02 21:34:30 -0400

    completed task 2 - j0 pprinter

--- high-level/hl.hs
+++ high-level/hl.hs
@@ -1,4 +1,9 @@
 data J0Expr = J0Num Integer | J0Plus J0Expr J0Expr | J0Mult J0Expr J0Expr
 
+j0pp :: J0Expr -> String
+j0pp (J0Num n) = show n
+j0pp (J0Plus lhs rhs) = "(+ " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
+j0pp (J0Mult lhs rhs) = "(* " ++ j0pp lhs ++ " " ++ j0pp rhs ++ ")"
+
 main :: IO ()
-main = putStrLn "Hello"
+main = putStrLn $ j0pp $ J0Plus (J0Num 1) (J0Mult (J0Num 2) (J0Num 3))
