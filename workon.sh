f2ceb063ae873522d2a379ef0793f8bf1e11e36b
erer1243 <>
2021-09-05 18:37:48 -0400

    workon script

---
+++ workon.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+node_count() {
+  bspc query --nodes | wc -l
+}
+
+NODES="$(node_count)"
+if [ $(hostname) = small ]; then
+	CHROMIUM=chromium
+	EDITCMD="alacritty -e vim high-level/hl.hs"
+else
+	CHROMIUM=chromium-freeworld
+	EDITCMD="sublime_text ."
+fi
+
+bspc node --presel-dir north --presel-ratio 0.8
+$CHROMIUM --new-window "https://jeapostrophe.github.io/courses/2021/fall/301/course/" >/dev/null 2>&1 &
+disown
+
+while [ $(node_count) = $NODES ]; do 
+	sleep 0.01
+done
+
+bspc node --presel-dir west --presel-ratio 0.6
+eval "$EDITCMD"
