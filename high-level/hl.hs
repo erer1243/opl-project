d5e3677fa2a4536343605d01640fd70df00c599f
erer1243 <>
2021-09-27 18:59:51 -0400

    completed task 23,24 - subst and big step j2

--- high-level/hl.hs
+++ high-level/hl.hs
@@ -8,6 +8,7 @@ import Data.String (IsString(..))
 import Data.List (intercalate)
 import System.Process (callCommand)
 import Control.Monad (forM_)
+import Data.Char (isUpper)
 
 -- prog ::= d... e
 data JProg = JProg [JDefine] JExpr deriving (Show, Eq)
@@ -49,6 +50,9 @@ type JVarRef = String
 -- to write SExprs like ["+", 1, 2]
 data SExpr = SESym String | SENum Integer | SEList [SExpr] deriving (Show, Eq)
 
+-- List of function definitions
+type Delta = [(JFnRef, JDefine)]
+
 pp :: JExpr -> String
 pp (JVal val) = case val of
     JNum n -> show n
@@ -62,107 +66,171 @@ pp (JVal val) = case val of
     JGt -> ">"
     JLtEq -> "<="
     JGtEq -> ">="
+    JFnRef s -> s
 pp (JIf cond e1 e2) = "(if " ++ pp cond ++ " " ++ pp e1 ++ " " ++ pp e2 ++ ")"
 pp (JApply head args) = "(" ++ pp head ++ " " ++ printedArgs ++ ")"
   where
     printedArgs = unwords $ map pp args
+pp (JVarRef s) = s
 
-interp :: JExpr -> JValue
-interp (JVal v) = v
-interp (JIf ec et ef) = if interp ec == JBool False then interp ef else interp et
-interp (JApply efn eargs) = delta vfn vargs
+-- Convenience alias
+interp = interpJProg
+
+-- Top level interp
+interpJProg :: JProg -> JValue
+interpJProg (JProg defns main) = interpJExpr delta main
   where
-    vfn = interp efn
-    vargs = map interp eargs
+    delta = map toPair defns
+    toPair def@(JDefine name _ _) = (name, def)
 
-delta :: JValue -> [JValue] -> JValue
-delta JPlus [JNum a, JNum b] = JNum (a + b)
-delta JMinus [JNum a, JNum b] = JNum (a - b)
-delta JMult [JNum a, JNum b] = JNum (a * b)
-delta JDiv [JNum a, JNum b] = JNum (a `div` b)
-delta JLtEq [JNum a, JNum b] = JBool (a <= b)
-delta JLt [JNum a, JNum b] = JBool (a < b)
-delta JEq [JNum a, JNum b] = JBool (a == b)
-delta JGt [JNum a, JNum b] = JBool (a > b)
-delta JGtEq [JNum a, JNum b] = JBool (a >= b)
-delta _ _ = error "Bad JApply"
-
-plug :: Context -> JExpr -> JExpr
-plug CHole e = e
-plug (CIf ctx et ef) e = JIf (plug ctx e) et ef
-plug (CApp before ctx after) e = case before of
-    -- The context is the prim
-    [] -> JApply (plug ctx e) after
-    -- The context is an argument to the prim
-    before ->
-        let (prim:argsBefore) = map JVal before
-            args = argsBefore ++ plug ctx e : after
-        in JApply prim args
-
-fr :: JExpr -> Maybe (Context, JExpr)
-fr (JVal _ ) = Nothing
-fr e@(JIf ec et ef) = case fr ec of
-    Nothing -> Just (CHole, e)
-    Just (ctx, rdx) -> Just (CIf ctx et ef, rdx)
-fr e@(JApply prim args) =
-    case span isJVal applyList of
-        (v, []) -> Just (CHole, e)
-        (v, e0:em) ->
-            -- The above branch guarantees all elements in v are JVal
-            let vals = map unwrapJVal v
-                Just (ctx, rdx) = fr e0
-            in Just (CApp vals ctx em, rdx)
+-- Interp a single JExpr
+interpJExpr :: Delta -> JExpr -> JValue
+interpJExpr delta expr = case expr of
+    JVal v -> v
+    JIf ec et ef -> if interpJExpr' ec == JBool False
+                    then interpJExpr' ef
+                    else interpJExpr' et
+    JApply efn eargs -> let vfn = interpJExpr' efn
+                            vargs = map interpJExpr' eargs
+                        in runDelta delta vfn vargs
+    JVarRef x -> error $ "Undefined variable " ++ x
   where
-    applyList = prim : args
-    isJVal (JVal _) = True
-    isJVal _ = False
+    interpJExpr' = interpJExpr delta
 
-smallStepInterp :: JExpr -> JValue
-smallStepInterp e = case fr e of
-    -- fr says that e is a JVal, so it is safe to unwrap it
-    Nothing -> unwrapJVal e
-    Just (ctx, rdx) -> smallStepInterp $ plug ctx $ step rdx
+-- Delta function, renamed since we now have the delta argument
+runDelta :: Delta -> JValue -> [JValue] -> JValue
+runDelta _ JPlus [JNum a, JNum b] = JNum (a + b)
+runDelta _ JMinus [JNum a, JNum b] = JNum (a - b)
+runDelta _ JMult [JNum a, JNum b] = JNum (a * b)
+runDelta _ JDiv [JNum a, JNum b] = JNum (a `div` b)
+runDelta _ JLtEq [JNum a, JNum b] = JBool (a <= b)
+runDelta _ JLt [JNum a, JNum b] = JBool (a < b)
+runDelta _ JEq [JNum a, JNum b] = JBool (a == b)
+runDelta _ JGt [JNum a, JNum b] = JBool (a > b)
+runDelta _ JGtEq [JNum a, JNum b] = JBool (a >= b)
+runDelta delta (JFnRef f) paramVals =
+    let ebody' = maybe (error f) (substArrow paramVals) (lookup f delta)
+    in interpJExpr delta ebody'
+runDelta _ p vs = error $ "Bad JApply " ++ show p ++ " " ++ show vs
 
--- Usage of fr in smallStepInterp guarantees all parts
--- of each JExpr passed to step are a JVal
-step :: JExpr -> JExpr
-step (JIf (JVal (JBool False)) _ ef) = ef
-step (JIf (JVal _) et _) = et
-step (JApply p v) = JVal $ delta (unwrapJVal p) (map unwrapJVal v)
-step _ = error "Bad step"
+-- Substitute multiple parameters into a JDefine body
+substArrow :: [JValue] -> JDefine -> JExpr
+substArrow vs (JDefine f xs ebody)
+    | length xs /= length vs = error $ f ++ ": bad param count"
+    | otherwise = foldr subst ebody (zip xs vs)
 
-desugar :: SExpr -> JExpr
-desugar (SENum n) = JVal $ JNum n
-desugar (SEList l) = case l of
+-- Substitute a single variable into a JExpr
+subst :: (JVarRef, JValue) -> JExpr -> JExpr
+subst var@(x, xv) e = case e of
+    e@(JVal _) -> e
+    JIf ec et ef -> JIf (subst' ec) (subst' et) (subst' ef)
+    JApply e0 em -> JApply (subst' e0) (map subst' em)
+    JVarRef y | y == x -> JVal xv
+              | otherwise -> JVarRef y
+  where
+    subst' = subst var
+
+-- plug :: Context -> JExpr -> JExpr
+-- plug CHole e = e
+-- plug (CIf ctx et ef) e = JIf (plug ctx e) et ef
+-- plug (CApp before ctx after) e = case before of
+--     -- The context is the prim
+--     [] -> JApply (plug ctx e) after
+--     -- The context is an argument to the prim
+--     before ->
+--         let (prim:argsBefore) = map JVal before
+--             args = argsBefore ++ plug ctx e : after
+--         in JApply prim args
+
+-- fr :: JExpr -> Maybe (Context, JExpr)
+-- fr (JVal _ ) = Nothing
+-- fr e@(JIf ec et ef) = case fr ec of
+--     Nothing -> Just (CHole, e)
+--     Just (ctx, rdx) -> Just (CIf ctx et ef, rdx)
+-- fr e@(JApply prim args) =
+--     case span isJVal applyList of
+--         (v, []) -> Just (CHole, e)
+--         (v, e0:em) ->
+--             -- The above branch guarantees all elements in v are JVal
+--             let vals = map unwrapJVal v
+--                 Just (ctx, rdx) = fr e0
+--             in Just (CApp vals ctx em, rdx)
+--   where
+--     applyList = prim : args
+--     isJVal (JVal _) = True
+--     isJVal _ = False
+
+-- smallStepInterp :: JExpr -> JValue
+-- smallStepInterp e = case fr e of
+--     -- fr says that e is a JVal, so it is safe to unwrap it
+--     Nothing -> unwrapJVal e
+--     Just (ctx, rdx) -> smallStepInterp $ plug ctx $ step rdx
+
+-- -- Usage of fr in smallStepInterp guarantees all parts
+-- -- of each JExpr passed to step are a JVal
+-- step :: JExpr -> JExpr
+-- step (JIf (JVal (JBool False)) _ ef) = ef
+-- step (JIf (JVal _) et _) = et
+-- step (JApply p v) = JVal $ delta (unwrapJVal p) (map unwrapJVal v)
+-- step _ = error "Bad step"
+
+-- Convenience alias
+desugar = desugarJProg
+
+-- The top level desugarer
+desugarJProg :: SExpr -> JProg
+desugarJProg (SEList (SESym "prog":body)) =
+    let defns = map desugarJDefine $ init body
+        main = desugarJExpr $ last body
+    in JProg defns main
+desugarJProg _ = undefined
+
+desugarJDefine :: SExpr -> JDefine
+desugarJDefine (SEList [SESym "define", SEList (SESym name:params), body]) =
+    JDefine name (map unwrapSESym params) (desugarJExpr body)
+  where
+    unwrapSESym (SESym s) = s
+    unwrapSESym _ = undefined
+desugarJDefine _ = undefined
+
+desugarJExpr :: SExpr -> JExpr
+desugarJExpr (SENum n) = JVal $ JNum n
+desugarJExpr (SEList l) = case l of
     -- +/* base cases
-    [SESym "+"] -> toJNum 0
-    [SESym "*"] -> toJNum 1
+    [SESym "+"] -> JVal $ JNum 0
+    [SESym "*"] -> JVal $ JNum 1
     -- +/* recursive cases
-    (plus@(SESym "+"):head:tail) -> JApply (JVal JPlus) [desugar head,
-                                                         desugar $ SEList $ plus : tail]
-    (mult@(SESym "*"):head:tail) -> JApply (JVal JMult) [desugar head,
-                                                         desugar $ SEList $ mult : tail]
+    (plus@(SESym "+"):head:tail) -> JApply (JVal JPlus) [desugarJExpr head,
+                                                         desugarJExpr $ SEList $ plus : tail]
+    (mult@(SESym "*"):head:tail) -> JApply (JVal JMult) [desugarJExpr head,
+                                                         desugarJExpr $ SEList $ mult : tail]
     -- negation
-    [SESym "-", SENum n] -> toJNum $ negate n
-    -- other prims
-    [sym@(SESym _), SENum lhs, SENum rhs] -> JApply (desugar sym) [toJNum lhs, toJNum rhs]
-    [SESym "if", ec, et, ef] -> JIf (desugar ec) (desugar et) (desugar ef)
+    [SESym "-", e] -> JApply (JVal JMult) [JVal (JNum (-1)), desugarJExpr e]
+    -- conditional
+    [SESym "if", ec, et, ef] -> JIf (desugarJExpr ec) (desugarJExpr et) (desugarJExpr ef)
+    -- other apply
+    (sym@(SESym _):tail) -> JApply (desugarJExpr sym) (map desugarJExpr tail)
+
+    -- Error case
     l -> error $ "bad SEList " ++ show l
-  where
-    toJNum = JVal . JNum
-desugar (SESym s) = JVal $ case s of
-    "+" -> JPlus
-    "-" -> JMinus
-    "*" -> JMult
-    "/" -> JDiv
-    "=" -> JEq
-    "<" -> JLt
-    ">" -> JGt
-    "<=" -> JLtEq
-    ">=" -> JGtEq
-    "true" -> JBool True
-    "false" -> JBool False
-    s -> error $ "bad SESym " ++ s
+desugarJExpr (SESym s) = case s of
+    -- Builtins
+    "+" -> JVal JPlus
+    "-" -> JVal JMinus
+    "*" -> JVal JMult
+    "/" -> JVal JDiv
+    "=" -> JVal JEq
+    "<" -> JVal JLt
+    ">" -> JVal JGt
+    "<=" -> JVal JLtEq
+    ">=" -> JVal JGtEq
+    "true" -> JVal $ JBool True
+    "false" -> JVal $ JBool False
+    -- Function or variable, depending on uppercase/lowercase
+    s@(c:_) | isUpper c -> JVal $ JFnRef s
+            | otherwise -> JVarRef s
+    -- Empty string error case
+    _ -> undefined
 
 -- Helper function that unwraps a JValue from a JExpr JVal variant
 -- In a couple places, we are guaranteed that a JExpr is a JVal
@@ -172,16 +240,14 @@ unwrapJVal :: JExpr -> JValue
 unwrapJVal (JVal v) = v
 unwrapJVal e = error $ "unwrapJVal " ++ show e
 
-checkJExprBig :: JExpr -> JValue -> Bool
-checkJExprBig expr ans = interp expr == ans
+-- checkJExprBig :: JProg -> JValue -> Bool
+-- checkJExprBig prog ans = interp prog == ans
 
-checkJExprSmall :: JExpr -> JValue -> Bool
-checkJExprSmall expr ans = smallStepInterp expr == ans
+-- checkJExprSmall :: JExpr -> JValue -> Bool
+-- checkJExprSmall expr ans = smallStepInterp expr == ans
 
-checkSExpr :: SExpr -> JValue -> Bool
-checkSExpr expr ans = checkJExprBig jExpr ans && checkJExprSmall jExpr ans
-  where
-    jExpr = desugar expr
+-- checkSExpr :: SExpr -> JValue -> Bool
+-- checkSExpr expr = checkJExprBig (desugarJProg expr)
 
 -- [(program, expected_answer)]
 tests :: [(SExpr, JValue)]
@@ -221,26 +287,33 @@ tests = [ (["prog", "<="], JLtEq)
                     ["CollatzHighest", 27, 0]], JNum 9232)
         ]
 
-runTests :: IO ()
-runTests = do
-    putStrLn "Running tests in HL code:"
-    let testResults = map (uncurry checkSExpr) tests
-    let numSuccesses = length $ filter id testResults
-    let numFailures = length tests - numSuccesses
-    putStrLn $ show numSuccesses ++ " successes and " ++ show numFailures ++ " failures"
+-- runTests :: IO ()
+-- runTests = do
+--     putStrLn "Running tests in HL code:"
+--     let testResults = map (uncurry checkSExpr) tests
+--     let numSuccesses = length $ filter id testResults
+--     let numFailures = length tests - numSuccesses
+--     putStrLn $ show numSuccesses ++ " successes and " ++ show numFailures ++ " failures"
 
-    putStrLn "Running tests in LL code:"
-    forM_ tests runTestInLL
+--     putStrLn "Running tests in LL code:"
+--     forM_ tests runTestInLL
+
+-- Convert JProg to rust code
+jpToLL :: JProg -> String
+jpToLL (JProg defns main) = "JProg(" ++ listToLL (map jdToLL defns) ++ "," ++ jeToLL main ++ ")"
+
+-- Convert JDefine to rust code
+jdToLL :: JDefine -> String
+jdToLL (JDefine name args body) = "JDefine(" ++ strToLL name ++ ","
+                                             ++ listToLL (map strToLL args) ++ ","
+                                             ++ jeToLL body ++ ")"
 
 -- Converts a single JExpr to low level rust code.
 jeToLL :: JExpr -> String
 jeToLL (JVal v) = "jval(" ++ jvToLL v ++ ")"
-jeToLL (JIf ec et ef) =
-    let commaSeparatedArgs = intercalate ", " (map jeToLL [ec, et, ef])
-    in "jif(" ++ commaSeparatedArgs ++ ")"
-jeToLL (JApply p args) =
-    let commaSeparatedArgs = intercalate ", " $ map jeToLL args
-    in "japply(" ++ jeToLL p ++ ", [" ++ commaSeparatedArgs ++ "])"
+jeToLL (JIf ec et ef) = "jif(" ++ listToLL (map jeToLL [ec, et, ef]) ++ ")"
+jeToLL (JApply p args) = "japply(" ++ jeToLL p ++ "," ++ listToLL (map jeToLL args) ++ ")"
+jeToLL (JVarRef s) = strToLL s
 
 -- Converts a single JValue to low level rust code.
 jvToLL :: JValue -> String
@@ -256,13 +329,20 @@ jvToLL v = "JValue::" ++ case v of
     JEq -> "JEq"
     JGt -> "JGt"
     JGtEq -> "JGtEq"
+    JFnRef s -> strToLL s
+
+listToLL :: [String] -> String
+listToLL strs = "List::from([" ++ intercalate ", " strs ++ "])"
+
+strToLL :: String -> String
+strToLL s = "\"" ++ s ++ "\""
 
 -- Takes a test and runs it in ll code, printing the result and if it failed
 runTestInLL :: (SExpr, JValue) -> IO ()
 runTestInLL (se, ans) = do
     -- Convert to source code
-    let je = desugar se
-    let jeLL = jeToLL je
+    let je = desugarJProg se
+    let jeLL = jpToLL je
     let ansLL = jvToLL ans
 
     -- Print message
@@ -286,7 +366,7 @@ runTestInLL (se, ans) = do
     callCommand "cargo run --quiet --manifest-path=../low-level/Cargo.toml"
 
 main :: IO ()
-main = runTests
+main = return ()
 
 -- Enable conversion from number literals into SENum
 -- Only fromInteger and negate are needed so the rest is left undefined
