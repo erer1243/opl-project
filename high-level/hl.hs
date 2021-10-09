9b5a7fbde86aaa374ee57de3cefacaf841620f9d
erer1243 <>
2021-10-09 16:06:43 -0400

    completed task 35 - factorial in lambda calculus

--- high-level/hl.hs
+++ high-level/hl.hs
@@ -181,7 +181,62 @@ tests = [ (1, JNum 1)
                      "add5", ["curriedAdd", 5],
                      "add10", ["curriedAdd", 10]],
                     ["add10", ["add5", ["add10", 100]]]], JNum 125)
+        -- Task 35 lambda calculus/factorial tests
+        , (task35Test ["numToJ3", ["factorial", "zero"]], JNum 1)
+        , (task35Test ["numToJ3", ["factorial", "one"]], JNum 1)
+        , (task35Test ["numToJ3", ["factorial", ["add1", "one"]]], JNum 2)
+        , (task35Test ["numToJ3", ["factorial", ["add1", ["add1", "one"]]]], JNum 6)
+        , (task35Test ["numToJ3", ["factorial", ["add1", ["add1", ["add1", "one"]]]]], JNum 24)
+        , (task35Test ["numToJ3", ["factorial", ["add1", ["add1", ["add1", ["add1", "one"]]]]]], JNum 120)
+        , (task35Test ["numToJ3", ["factorial", ["add1", ["add1", ["add1", ["add1", ["add1", "one"]]]]]]], JNum 720)
         ]
+
+-- Convenience alias to make lambda code shorter
+λ :: SExpr
+λ = "lambda"
+
+-- Takes an sexpr and puts it into the task 35
+-- lambda calculus "standard library". For convenience of
+-- testing multiple parts of my task 35 code.
+task35Test :: SExpr -> SExpr
+task35Test ebody =
+    ["let*", [
+              -- Booleans
+              "true_", [λ, ["t"], [λ, ["f"], "t"]],
+              "false_", [λ, ["t"], [λ, ["f"], "f"]],
+              "if_", [λ, ["c"], "c"],
+              -- Pairs
+              "pair", [λ, ["l"], [λ, ["r"], [λ, ["s"],
+                        [["s", "l"], "r"] ]]],
+              "fst", [λ, ["p"], ["p", "true_"]],
+              "snd", [λ, ["p"], ["p", "false_"]],
+              -- Numbers and numeric operations
+              "zero", [λ, ["f"], [λ, ["x"], "x"]],
+              "one", [λ, ["f"], [λ, ["x"], ["f", "x"]]],
+              "zero?", [λ, ["n"], [["n", [λ, ["x"], "false_"]], "true_"]],
+              "add", [λ, ["n"], [λ, ["m"], [λ, ["f"], [λ, ["x"],
+                       [["n", "f"], [["m", "f"], "x"]] ]]]],
+              "mult", [λ, ["n"], [λ, ["m"], [λ, ["f"], [λ, ["x"],
+                        [["n", ["m", "f"]], "x"] ]]]],
+              "add1", [λ, ["n"], [["add", "n"], "one"] ],
+              "sub1", [λ, ["n"],
+                        ["fst", [["n", [λ, ["p"], [["pair", ["snd", "p"]], ["add1", ["snd", "p"]]]]],
+                                       [["pair", "zero"], "zero"]]]],
+              -- Z combinator
+              "Z", [λ, ["f"], [[λ, ["x"], ["f", [λ, ["v"], [["x", "x"], "v"]]]],
+                               [λ, ["x"], ["f", [λ, ["v"], [["x", "x"], "v"]]]]] ],
+              -- Factorial
+              "make-factorial", [λ, ["fac"], [λ, ["n"],
+                                  [[[["if_", ["zero?", "n"]],
+                                       [λ, ["x"], "one"] ],
+                                       [λ, ["x"], [["mult", "n"], ["fac", ["sub1", "n"]]]] ],
+                                   "zero"] ]],
+              "factorial", ["Z", "make-factorial"],
+              -- Convert a church encoded number to a J3 number
+              "numToJ3", [λ, ["n"], [["n", [λ, ["m"], ["+", "m", 1]]], 0]]
+             ],
+             ebody
+    ]
 
 runTests :: IO ()
 runTests = forM_ tests runTestInLL
@@ -228,7 +283,7 @@ runTestInLL (se, ans) = do
 
     -- Print message
     putStrLn "========================="
-    putStrLn $ "test=" ++ show se
+    --putStrLn $ "test=" ++ show se
     putStrLn $ "expr=" ++ pp je
     putStrLn $ "expecting=" ++ show ans
 
